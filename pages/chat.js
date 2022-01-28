d419b7e841af474539e42a3bfe377e0a3c57283a
Peu <peustratt@gmail.com>
2022-01-27 23:37:31 -0300

    animação do loader

--- pages/chat.js
+++ pages/chat.js
@@ -112,6 +112,7 @@ export default function ChatPage() {
 					inset: '0',
 					zIndex: '1',
 				}}></div>}
+				{!hasLoaded && <Spinner customText={"Eita fetch demorado..."} />}
 				<Header />
 				<Box
 					styleSheet={{
@@ -302,6 +303,67 @@ function MessageList(props) {
 			`}
 			</style> */}
 		</>
+	)
+}
+
+function Spinner(props) {
+	return (
+		<>
+			<div className="spinner-container">
+
+				<svg width="100%" viewBox="0 0 276 276" fill="none" xmlns="http://www.w3.org/2000/svg" >
+					<g id="spinner">
+						<circle id="bottom" cx="138" cy="138" r="114" stroke="#E4E7EB" stroke-width="18" />
+						<circle id="upper" cx="138" cy="138" r="123" stroke="#57AE5B" stroke-width="30" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="373 100" style={{ animationDuration: props.speed + "s" }} />
+					</g>
+				</svg>
+				<p>{props.customText}</p>
+			</div>
+			<style jsx>{`
+				.spinner-container {
+					z-index: 99;
+					position: fixed;
+					width: 100px;
+					padding: 10px;
+					top: 50%;
+					display: flex;
+					justify-content: center;
+					align-content: center;
+					flex-direction: column;
+					left: 50%;
+					margin-top: -100px; 
+					margin-left: -50px;
+					border-radius: 0.5rem;
+				}
+
+				.spinner-container p
+				{
+					text-align: center;
+					margin: 5px 0 0 0;
+				}
+
+				#upper
+
+				{
+					animation: rotate;
+					animation-duration: 3s;
+					animation-iteration-count: infinite;
+					transform-origin: center;
+				}
+
+				@keyframes rotate {
+					0%
+					{
+						transform: rotateZ(0deg);
+					}
+					100%
+					{
+						transform: rotateZ(360deg);
+					}
+				}
+			`}</style>
+		</>
+
 	)
 }
 
